beb613793034a00279e6681389d9a7ab0aae02dc
claudeomosa <claudeomosa@gmail.com>
2022-12-15 07:54:43 +0300

    refactor: remove println! statements from main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing the arguments: {}", err);
         process::exit(1)
     });
-    println!("searching for {}, in {}", config.query, config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("ERROR: {e}");
         process::exit(1)
     }
 }
