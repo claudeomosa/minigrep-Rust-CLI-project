aa52c18ec11741ad05d0309412e5816ea09b52e4
claudeomosa <claudeomosa@gmail.com>
2022-12-15 07:56:43 +0300

    feat: add search logic use test driven development ie writing tests

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,35 @@ impl Config {
 }
 // this function takes a Config instance and prints file content from the provided fields 
 pub fn run(config: Config) -> Result<(), Box<dyn Error >> {
-    let file_content = fs::read_to_string(config.file_path)?;
-    println!("with text: \n ------------- \n{} \n -------------", file_content);
+    let file_contents = fs::read_to_string(config.file_path)?;
+    
+    for line in search(&config.query, &file_contents){
+        println!("{line}")
+    }
+
     Ok(())
+}
+
+// tests module
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive"],  search(query, contents))
+    }
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut vector = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            vector.push(line)
+        }
+    }
+    vector
 }
