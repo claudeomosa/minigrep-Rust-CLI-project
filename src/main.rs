fea7d05473ff183f04f63edf0cd97ac7c0866888
claudeomosa <claudeomosa@gmail.com>
2022-12-18 09:25:29 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use minigrep::Config;
 fn main() {
     println!("-------------------------------------------------");
     // this reads arguments and stores them in a vector called args
-    // let args: Vec<String> = env::args().collect();
 
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
